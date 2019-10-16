c7da867c7599c8bb7d559b4fb769f89120ed176a
Allan Segovia Spadini <allan.spadini@gmail.com>
2019-10-16 14:32:05 -0300

    Add files via upload

--- Banco.java
+++ Banco.java
@@ -1,17 +1,24 @@
 
 import java.io.File;
 import weka.core.Instances;
-import weka.core.converters.CSVLoader;
+//import weka.core.converters.CSVLoader;
 import weka.classifiers.trees.J48;
+import weka.core.converters.ArffLoader;
 
 
 public class Banco {
 	public static void main(String[] args) throws Exception {
 		
 		//Carrega o arquivo CSV
-		CSVLoader loader = new CSVLoader();
-		loader.setSource(new File("bank_classificador.csv"));
-		Instances dado = loader.getDataSet();
+		//CSVLoader loader = new CSVLoader();
+		//loader.setSource(new File("bank_classificador.csv"));
+		//Instances dado = loader.getDataSet();
+		
+		ArffLoader carrega = new ArffLoader();
+		carrega.setSource(new File("banco_transformado.arff"));
+		Instances dado = carrega.getDataSet();
+		
+		
 		//Indica qual o atributo vamos classificar 
 		dado.setClassIndex(dado.numAttributes()-1);
 		
@@ -19,7 +26,7 @@ public class Banco {
 		J48 arvore = (J48) weka.core.SerializationHelper.read("modelo_j48.model");
 		
 		//Pega uma linha do arquivo e classifica
-		double resultado = arvore.classifyInstance(dado.instance(0));
+		double resultado = arvore.classifyInstance(dado.instance(8));
 		System.out.println("resultado da classificação: " + resultado);
 		
 		
